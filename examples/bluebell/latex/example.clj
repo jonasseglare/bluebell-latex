5ec2693a8991183d4e89fbabd5b2947930050047
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-06-22 08:36:17 +0200

    good

--- examples/bluebell/latex/example.clj
+++ examples/bluebell/latex/example.clj
@@ -9,4 +9,24 @@
                [:maketitle]
                ["A line here\n" "Another line"]]])
 
-(def compiled (io-utils/display testdoc))
+(defn doc-demo []
+  (io-utils/display testdoc))
+
+(defn tikz-demo []
+  (io-utils/display
+   [:begin "tikzpicture" :body
+    [[:draw {:style "dashed"}]
+     "(2, .5) circle (0.5);"
+
+     [:draw {:fill "green!50"}]
+     "(1, 1)" "ellipse (.5 and 1);"
+
+     ]]))
+;; begin{tikzpicture}
+;; \draw[style=dashed] (2,.5) circle (0.5);
+;; \draw[fill=green!50] (1,1)
+;; ellipse (.5 and 1);
+;; \draw[fill=blue] (0,0) rectangle (1,1);
+;; \draw[style=thick]
+;; (3,.5) -- +(30:1) arc(30:80:1) -- cycle;
+;; \end{tikzpicture}
