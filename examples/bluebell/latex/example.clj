f6ddde64a0a389298b83208881942b644d040a5d
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-06-22 08:29:22 +0200

    bra va

--- examples/bluebell/latex/example.clj
+++ examples/bluebell/latex/example.clj
@@ -3,8 +3,10 @@
             [bluebell.latex.io-utils :as io-utils]))
 
 (def testdoc [[:documentclass :opt "a4paper" "article"]
-           [:begin "document"
-            :body
-            ["A line here\n" "Another line"]]])
+              [:title "Mjaoelimjao"]
+              [:begin "document"
+               :body
+               [:maketitle]
+               ["A line here\n" "Another line"]]])
 
 (def compiled (io-utils/display testdoc))
