3775d9553b725d787c07506112db130865594b1b
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-07-04 09:14:33 +0200

    render

---
+++ src/bluebell/latex/render.clj
@@ -0,0 +1,8 @@
+(ns bluebell.latex.render
+  (:require [clojure.spec :as spec]
+            [bluebell.utils.string :as string]
+            [bluebell.utils.indent :as indent]
+            [bluebell.utils.defmultiple :as defmultiple]))
+
+(defn full-compile [x]
+  (indent/render x))
